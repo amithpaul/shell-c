087afac176d12450904a8f8791b066dd67aded8b
Amith Paul <amithpaulsiju@gmail.com>
2025-05-25 11:06:28 +0530

    [any message]

--- src/main.c
+++ src/main.c
@@ -11,6 +11,6 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
-  printf("%s: command not found\n", input);
+  printf("%s: command not found", input);
   return 0;
 }
